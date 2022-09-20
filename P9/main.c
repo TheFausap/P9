6cba85bad70245a8244a0ce62de756e631e68e08
Fausto Saporito <fausto.saporito@thyssenkrupp.com>
2022-09-20 12:55:47 +0200

    Initial Commit

---
+++ P9/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  P9
+//
+//  Created by Fausto Saporito on 20/09/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
